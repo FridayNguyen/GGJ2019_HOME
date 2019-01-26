1d9884c7037517458df4ff5933d640ad3438b229
Filip Kestner <filip.kestner@gmail.com>
2019-01-26 14:43:04 -0800

    Added Basic Zombie
    ~ Prefab of Basic Zombie
    ~ 2x Scripts (Chase, Zombie_Main)
    ~ 2x Materials

---
+++ Assets/Scripts/Chase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Chase : MonoBehaviour
+{
+    UnityEngine.AI.NavMeshAgent nav_agent;
+    public Transform goal; //Nav System, don't fuck with
+    private int health; //Only able to access through the set method
+    void OnCollisionEnter(Collision col){
+
+        if(col.gameObject.name == "Player"){
+            Destroy(col.gameObject); //Destroy player on collision
+        }
+
+        if(col.gameObject.name == "Bullet"){ //Bullet object name
+            health--; //This reduces health by one in the main script
+        }
+    }
+    void Start()
+    {
+        nav_agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav_agent.destination = goal.position;
+        if(health == 0){
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void setHealth(int new_health){
+        health = new_health; 
+    }
+
+
+}
