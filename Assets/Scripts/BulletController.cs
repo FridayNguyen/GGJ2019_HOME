9fe3e748dbbc5381780b71c9ebcd87361408a7b3
Hung D Nguyen <hungdacnguyen99@gmail.com>
2019-01-27 02:08:55 -0800

    improved lighting, bullet effect

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class BulletController : MonoBehaviour
 {
     public float speed;
+    private float death_timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        death_timer += Time.deltaTime;
         transform.Translate(Vector3.right * speed * Time.deltaTime);
-        
+
+
+        if (death_timer > 4F) { Destroy(gameObject); }
+
     }
 }
