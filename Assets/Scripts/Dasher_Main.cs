0f2606034a0ec545ba52d9af0f7f0f4130523bba
Filip Kestner <filip.kestner@gmail.com>
2019-01-27 03:26:00 -0800

    Zombie Bois Coming Through
    Basic Boi
    Dash Boi
    Brute Boi

---
+++ Assets/Scripts/Dasher_Main.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Dasher_Main : MonoBehaviour
+{
+
+    Ray targetRay;
+    RaycastHit rayHit; 
+    Rigidbody rigBod;
+    Renderer rend;
+
+    private bool flicker; 
+    private bool dashing;
+    public int dash_force;
+    private Color default_color = new Color(36,36,93);
+
+    Vector3 dash_vector;
+
+    private bool track_time; 
+    private bool time_cooldown;
+    private float dash_attack_timer;
+    private float dash_attack_cooldown;
+
+    public float range;
+
+    private NavMeshAgent NavMeshA;
+
+    // Start is called before the first frame update
+
+
+    void dash(){
+        //rigBod.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+
+        dash_vector = dash_force*transform.forward;
+        rigBod.AddForce(dash_vector); 
+
+    }
+
+    void dash_flicker(){
+
+
+    }
+    
+
+    void Start()
+    {
+        gameObject.GetComponent<Chase>().setHealth(1); //Calling set health var 
+        NavMeshA = GetComponent<NavMeshAgent>();
+        rigBod = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+        dashing = false; 
+        
+    }
+
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+
+        if(track_time){
+            dash_attack_timer += Time.deltaTime;
+        }
+        //dash_attack_cooldown += Time.deltaTime;
+
+        //Debug.Log(dash_attack_timer);
+        Debug.DrawRay(transform.position,transform.forward*range, Color.red); //Comment out to remove red line
+
+        if(Physics.Raycast(transform.position,transform.forward,range))
+        {
+
+            Debug.Log("TRIGGGER!");
+            track_time = true;
+            if (dash_attack_timer < .5f) { 
+                NavMeshA.velocity = new Vector3(0,0,0);
+                }
+            
+
+            
+        }
+
+        
+        if(dash_attack_timer > .5f){
+            NavMeshA.velocity = dash_force*transform.forward;
+            dash_attack_timer = 0;
+            Debug.Log("TIMER RESET!");
+            track_time = false;
+
+
+        }
+
+
+
+        
+
+
+        
+        
+
+
+
+    }
+
+
+
+}
+
+    
+
+
+
+    
+
---
+++ Assets/Scripts/Zombie_Brute_Main.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie_Brute_Main : MonoBehaviour
+{
+    // Health variable of brute
+    public int health = 5;
+
+    Renderer rend;
+
+    Color myColor;
+
+    void Awake()
+    {
+        gameObject.GetComponent<Chase>().setHealth(health); //Calling set health var to 10 because it must take more hits
+    
+        rend = gameObject.GetComponent<Renderer>();
+
+        myColor = rend.material.color;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(flicker_delay());
+        Debug.Log("SECOND TRIGGER!");
+    }
+
+    IEnumerator flicker_delay(){
+
+        Debug.Log("TRIGGA ME BABY!");
+        //531482
+        //rgb(83,20,130)
+        //rend.material.color = new Color(83,20,130,1);
+        rend.material.SetColor("_Color", new Color(255,0,0)*.05f);
+        yield return new WaitForSeconds(.25f);
+        rend.material.color = myColor;
+        
+    }
+}
