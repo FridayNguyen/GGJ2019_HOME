cace17186703c94f7d8cb279ae3a996b2049dda9
Spencer Fong <34435767+fongse99@users.noreply.github.com>
2019-01-27 02:43:49 -0800

    Revert "Revert "adds text boxes""
    This reverts commit c71d89ec25661e5c02050d7f8d585b5f31f3b5c0.

---
+++ Assets/Scripts/TextTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTrigger : MonoBehaviour
+{
+    [SerializeField] private Text customText;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customText.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customText.enabled = false;
+        }
+    }
+}
